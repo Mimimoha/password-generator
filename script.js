29d0744cfbfd2dbccbf4151064131989bdb4ca4f
Mimimoha <mohamedm1987@gmail.com>
2022-09-17 20:30:31 -0500

    final commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var useLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var length = [26] 
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
 
 // password of 8 to 128 characters with option of lowercase, uppercase, numeric, and/or special characters
 
@@ -19,32 +17,23 @@ function numeric(min,max){
   usenumeric = confirm( "do you want numaric in your password?")
   if (numeric) {
   confirm === usenumeric};
-  var random = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log(numeric)
+  console.log(usenumeric)
 }
-  
+
 // ask the user if they want lower case letters in the password
-function useLowerCase(useLower){
+function useLowerCase(){
   useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
   if (useLowerCase) {
-  confirm === useLowerCaseLtrs};
-  for(var i=0; i< length; i++){
-    useLowerCase += useLower.charAt(Math.floor(Math.random() * 
-    length));
-
-  return useLowerCase;
-  }
+  confirm === useLowerCaseLtrs}
+  console.log(useLowerCaseLtrs)
 }
-
+ 
 // ask user if they want uppercase letters in the password 
 function useUpperCase(){
   useUpperCaseltrs = confirm("Do you want uppercase letter in the password?");
   if (useUpperCase) {
-  confirm === useUpperCaseltrs;
- } 
- var random = useUpperCase(0, uppercase.length -1)
-console.log(useUpperCaseltrs)
-  
+  confirm === useUpperCaseltrs;}
+  console.log(useUpperCaseltrs)
 }
 // as user if the want special characters added in their password
 
@@ -56,13 +45,23 @@ function specialchatacters(){
   console.log(specialchatactersadded)
 }
 function generatePassword() {
+  var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 20;
+  var password = "";
+for (var i = 0; i <= passwordLength; i++) {
+ var randomNumber = Math.floor(Math.random() * characters.length);
+ password += characters.substring(randomNumber, randomNumber +1);
+}
+}
+ function generatePassword() {
   console.log("start here");
   howManyCharacters();
   useLowerCase();
   useUpperCase();
-  numeric(0,100);
+  numeric();
   specialchatacters();
-  return "finalpassword"
+
+return "final password"
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
