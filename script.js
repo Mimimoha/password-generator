7c6ed52693b2b708c427591b97790a4a527a623d
Mimimoha <mohamedm1987@gmail.com>
2022-09-22 09:55:18 -0500

    final corrections

--- script.js
+++ script.js
@@ -1,68 +1,84 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var numOfCharacters = 0 
+var option = []
+finalpassword = []
+var numbers = ["0", "1","2", "3", "4"]
+var lowercase = ["a", "b", "c"]
+var uppercase = ["A","B","C"]
+var special = ["!","@","#"]
 
 // password of 8 to 128 characters with option of lowercase, uppercase, numeric, and/or special characters
+function randomize(array){
+  var randomIndex = Math.floor(Math.random()* array.length)
+  var indexValue = array[randomIndex]
+  return indexValue
+}
 
 // ask the user how many characters should be in the password
 function howManyCharacters() {
   numOfCharacters = parseInt( prompt("How many characters?") );
-if (howManyCharacters < 8 || howManyCharacters > 128) { 
-alert( "your password must be between 8 to 128 characters, please try again");
-}
 }
 
 // ask user if they want numaric in their password
 function numeric(min,max){
-  usenumeric = confirm( "do you want numaric in your password?")
-  if (numeric) {
-  confirm === usenumeric};
+  var usenumeric = confirm( "do you want numaric in your password?")
+  if ( usenumeric === true ) { 
+    option = option.concat(numbers)
+    var min=0
+    var max = 10
+    var random = Math.floor(Math.random() * (max - min + 1) + min)
+  }
   console.log(usenumeric)
+
 }
 
 // ask the user if they want lower case letters in the password
 function useLowerCase(){
-  useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
-  if (useLowerCase) {
-  confirm === useLowerCaseLtrs}
+  var useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
+  if (useLowerCaseLtrs) {
+   option = option.concat(lowercase)}
   console.log(useLowerCaseLtrs)
 }
  
 // ask user if they want uppercase letters in the password 
 function useUpperCase(){
-  useUpperCaseltrs = confirm("Do you want uppercase letter in the password?");
-  if (useUpperCase) {
-  confirm === useUpperCaseltrs;}
+  var useUpperCaseltrs= confirm("Do you want uppercase letter in the password?");
+  if (useUpperCaseltrs) {
+  option = option.concat (uppercase) }
   console.log(useUpperCaseltrs)
 }
 // as user if the want special characters added in their password
 
 function specialchatacters(){
-  specialchatactersadded = confirm ("Do you want special characters added to your password?")
-  if (specialchatacters) {
-  confirm === specialchatactersadded;
+  var specialchatactersadded = confirm ("Do you want special characters added to your password?")
+  if (specialchatactersadded) {
+ option = option.concat (special)
  } 
   console.log(specialchatactersadded)
 }
-function generatePassword() {
-  var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 20;
-  var password = "";
-for (var i = 0; i <= passwordLength; i++) {
- var randomNumber = Math.floor(Math.random() * characters.length);
- password += characters.substring(randomNumber, randomNumber +1);
-}
-}
+
+
  function generatePassword() {
   console.log("start here");
   howManyCharacters();
+  if (numOfCharacters < 8 || numOfCharacters > 128) { 
+    alert( "your password must be between 8 to 128 characters, please try again");
+    howManyCharacters();
+    } 
   useLowerCase();
   useUpperCase();
   numeric();
-  specialchatacters();
-
-return "final password"
+  specialchatacters()
+ console.log (option)
+ for(var i = 0; i < numOfCharacters; i++){
+finalpassword.push(randomize(option))
+console.log(finalpassword)
+ }
+return finalpassword.join("")
 }
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -73,3 +89,10 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// for as many times as numofcharacters
+// max = option.length
+// max = 0
+// var random = // put math here
+// password.push(option[random])
+
